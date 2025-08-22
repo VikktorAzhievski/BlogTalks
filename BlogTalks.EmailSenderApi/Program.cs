17f8be33f9e20c6124921a806884b7d3d28e19e1
SCALEFOCUS\viktor.azhievski <viktor.azhievski@scalefocus.com.mk>
2025-08-22 14:42:56 +0200

    Add bacgroundService

--- BlogTalks.EmailSenderApi/Program.cs
+++ BlogTalks.EmailSenderApi/Program.cs
@@ -2,25 +2,31 @@ using BlogTalks.EmailSenderApi.DTO;
 using BlogTalks.EmailSenderApi.Service;
 using BlogTalks.EmailSenderApi.Services;
 using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.Configure<RabbitMQSettingseEmailSender>(builder.Configuration.GetSection("RabbitMqSettings"));
+builder.Services.AddSingleton(builder.Configuration
+    .GetSection("RabbitMqSettings")
+    .Get<RabbitMQSettingseEmailSender>());
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi(); 
+    app.MapOpenApi();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "EmailSender API v1");
-    }); 
+    });
 }
 
 app.UseHttpsRedirection();
-
 
 app.MapPost("/send", async (EmailDto request, IEmailSender emailSender) =>
 {
---
+++ BlogTalks.EmailSenderApi/Service/RabbitMQEmailBackgroundService.cs
@@ -0,0 +1,79 @@
+﻿using BlogTalks.EmailSenderApi.DTO;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+namespace BlogTalks.EmailSenderApi.Service
+{
+    public class RabbitMQEmailBackgroundService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly BlogTalks.EmailSenderApi.DTO.RabbitMQSettingseEmailSender _rabbitSettings;
+        private readonly ILogger<RabbitMQEmailBackgroundService> _logger;
+        private IConnection _connection;
+        private IChannel _channel;
+        private readonly IEmailSender _emailSender;
+
+        public RabbitMQEmailBackgroundService(IServiceScopeFactory serviceScopeFactory, IOptions<BlogTalks.EmailSenderApi.DTO.RabbitMQSettingseEmailSender> rabbitSettings, ILogger<RabbitMQEmailBackgroundService> logger, IEmailSender emailSender)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+            _rabbitSettings = rabbitSettings.Value;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+
+        private async Task InitRabbitMQAsync(EmailDto emailDto)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _rabbitSettings.RabbitURL,
+                UserName = _rabbitSettings.Username,
+                Password = _rabbitSettings.Password
+            };
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
+
+            await channel.ExchangeDeclareAsync(_rabbitSettings.ExchangeName, _rabbitSettings.ExchhangeType, durable: true);
+            await channel.QueueDeclareAsync(_rabbitSettings.QueueName, durable: true, exclusive: false, autoDelete: false);
+            await channel.QueueBindAsync(_rabbitSettings.QueueName, _rabbitSettings.ExchangeName, _rabbitSettings.RouteKey);
+            await _channel.BasicQosAsync(0, 1, false);
+
+            _connection.ConnectionShutdownAsync += RabbitMQ_ConnectionShutdown;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            consumer.ReceivedAsync += async (ch, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                var emailDto = JsonConvert.DeserializeObject<EmailDto>(message);
+
+                await _emailSender.SendAsync(emailDto);
+                await _channel.BasicAckAsync(ea.DeliveryTag, false);
+            };
+            consumer.ShutdownAsync += OnConsumerShutdown;
+            consumer.RegisteredAsync += OnConsumerRegistered;
+            consumer.UnregisteredAsync += OnConsumerUnregistered;
+
+            await _channel.BasicConsumeAsync(_rabbitSettings.QueueName, autoAck: false, consumer);
+        }
+        public override void Dispose()
+        {
+            _channel.CloseAsync();
+            _connection.CloseAsync();
+            base.Dispose();
+        }
+
+        private Task OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) => Task.CompletedTask;
+        private Task OnConsumerUnregistered(object sender, ConsumerEventArgs e) => Task.CompletedTask;
+        private Task OnConsumerRegistered(object sender, ConsumerEventArgs e) => Task.CompletedTask;
+        private Task OnConsumerShutdown(object sender, ShutdownEventArgs e) => Task.CompletedTask;
+        private Task RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) => Task.CompletedTask;
+    }
+}
